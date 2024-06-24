ea83b6b2aab6f5044de84b5a6ad032b70a9a6994
ISEUL <sgktn0204@gmail.com>
2024-06-24 16:11:22 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17980 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        Double A = sc.nextDouble();
+        Double B = sc.nextDouble();
+        System.out.print(A / B);
+    }
+}
