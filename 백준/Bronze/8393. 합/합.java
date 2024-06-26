3f458c6a062de9d5f535695aeb48582ca97f1708
ISEUL <sgktn0204@gmail.com>
2024-06-26 10:34:16 +0900

    [Bronze V] Title: 합, Time: 216 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,18 @@
+/*
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+*/
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        System.out.print(sum);
+    }
+}
