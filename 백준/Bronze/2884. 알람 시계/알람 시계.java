4272e42dca1e2d9c8d5f99953ca84958a10dd558
ISEUL <sgktn0204@gmail.com>
2024-07-02 08:55:52 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -4,10 +4,9 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String time = br.readLine();
-        int index = time.indexOf(" ");
-        int H = Integer.parseInt(time.substring(0,index));   // 시간 
-        int M = Integer.parseInt(time.substring(index + 1)); // 분
+        String[] time = br.readLine().split(" ");
+        int H = Integer.parseInt(time[0]);   // 시간 
+        int M = Integer.parseInt(time[1]); // 분
         
         if(M < 45)
         {
