f80a73643f749ee98bf47e1255eeda6606f766c6
ISEUL <sgktn0204@gmail.com>
2025-01-18 13:12:44 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		System.out.print( A > B ? A : B);     
+    }
+}
