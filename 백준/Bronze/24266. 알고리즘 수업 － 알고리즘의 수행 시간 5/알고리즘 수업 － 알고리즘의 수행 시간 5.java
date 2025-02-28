67a5ee4d10dc442a05bbb579d62295f9f9013e0e
ISEUL <sgktn0204@gmail.com>
2025-02-28 12:51:45 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        long n = Long.parseLong(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        System.out.println(n * n * n);
+        System.out.println(3);
+    }
+}
