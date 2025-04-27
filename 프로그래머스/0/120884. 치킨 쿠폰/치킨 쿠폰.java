d0cb966fe47cfa96a500c10e78d0568432b27e48
ISEUL <sgktn0204@gmail.com>
2025-04-28 07:36:12 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 86.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        
+        while(chicken >= 10) {
+            int service = chicken / 10; 
+            answer += service;
+            chicken = service + (chicken % 10); 
+        }
+        
+        return answer;
+    }
+}
