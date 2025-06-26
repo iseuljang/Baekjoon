df35d15b7a8b41c3209a7360125131012131abb5
ISEUL <sgktn0204@gmail.com>
2025-06-27 07:35:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 14.70 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String front = my_string.substring(0, s);
+        String middle = new StringBuilder(my_string.substring(s, e + 1)).reverse().toString();
+        String back = my_string.substring(e + 1);
+        
+        return front + middle + back;
+    }
+}
