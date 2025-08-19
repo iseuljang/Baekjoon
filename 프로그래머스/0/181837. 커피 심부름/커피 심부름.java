20ebe81101d19f2db8a6e51c0fa535d4d580dee2
ISEUL <sgktn0204@gmail.com>
2025-08-20 07:40:20 +0900

    [level 0] Title: 커피 심부름, Time: 0.67 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        
+        for(String o : order) {
+            if(o.contains("americano") || o.equals("anything")) {
+                answer += 4500;
+            }else if(o.contains("cafelatte")) {
+                answer += 5000;
+            }
+        }
+        
+        return answer;
+    }
+}
