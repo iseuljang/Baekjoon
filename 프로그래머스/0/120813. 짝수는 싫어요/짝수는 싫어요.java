1a29a4c862af244c47ee44c82c3ff3c6bf8c1725
ISEUL <sgktn0204@gmail.com>
2025-08-25 07:37:14 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n) {
+        int size = (n + 1) / 2;
+        int[] answer = new int[size];
+
+        int index = 0;
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 == 1) { 
+                answer[index++] = i;
+            }
+        }
+        return answer;
+    }
+}
