3e725ba160a1090100d8cdab28fbb1c9362ff1c0
ISEUL <sgktn0204@gmail.com>
2025-10-05 08:55:17 +0900

    [level 0] Title: 옹알이 (1), Time: 0.75 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] words = {"aya", "ye", "woo", "ma"};
+        
+        for(String b : babbling) {
+            String temp = b;
+            for(String w : words) {
+                temp = temp.replace(w, " ");  
+            }
+            temp = temp.replace(" ", ""); 
+            
+            if(temp.equals("")) { 
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
